57e06faa48b9364419c72c6f48cafd8eac4d6e85
Martin Comina <martin.comina@bodleian.ox.ac.uk>
2014-01-22 15:53:56 +0000

    Make filename match that used in processing script, for next time.

--- convert/olddata/download.sh
+++ convert/olddata/download.sh
@@ -2,6 +2,6 @@ echo 'You must pass the username who has access to the export.'
 user="$1"
 server="orastats.bodleian.ox.ac.uk"
 path="/opt/statmaker/export"
-scp -C "$user@$server:$path/actions.tsv" action.tsv
+scp -C "$user@$server:$path/actions.tsv" actions.tsv
 scp -C "$user@$server:$path/visits.tsv" visits.tsv
 scp -C "$user@$server:$path/links.tsv" links.tsv
