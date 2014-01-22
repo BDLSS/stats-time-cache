41d5f4d5aff73ed449143041831d3d35ee41f9e6
Martin Comina <martin.comina@bodleian.ox.ac.uk>
2014-01-22 11:43:06 +0000

    Add script to save needing to do the export manually again.

---
+++ convert/olddata/export.sh
@@ -0,0 +1,8 @@
+echo 'Export data from Piwik to TSV files.'
+echo 'This will fail if user stat-export does not have select access.'
+echo 'Exporting links.'
+mysql piwik --user=stat_export -e 'SELECT * FROM piwik_log_link_visit_action' > links.tsv
+echo 'Export actions.'
+mysql piwik --user=stat_export -e 'SELECT * FROM piwik_log_action' > actions.tsv
+echo 'Export visits.'
+mysql piwik --user=stat_export -e 'SELECT * FROM piwik_log_visit' > visits.tsv
