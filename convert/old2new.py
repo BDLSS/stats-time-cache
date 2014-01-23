9c859cccb66b7379ab0450164f23ee2fb1a1a73a
Martin Comina <martin.comina@bodleian.ox.ac.uk>
2014-01-23 13:47:16 +0000

    Start classes to control processing files.

--- convert/old2new.py
+++ convert/old2new.py
@@ -2,15 +2,119 @@
 # to a new version with an option to convert certain data.
 import os
 import logging
+import csv
+import re #To detect UUIDs in urls
 
-DIR_ROOT = os.getcwd()
-DIR_OLD = os.path.join(DIR_ROOT, 'olddata')
-DIR_NEW = os.path.join(DIR_ROOT, 'newdata')
+class Converter(object):
+    '''Control the conversion process.'''
+    def __init__(self):
+        self.DIR_ROOT = os.getcwd()
+        self.DIR_OLD = os.path.join(self.DIR_ROOT, 'olddata')
+        self.DIR_NEW = os.path.join(self.DIR_ROOT, 'newdata')
+        self.FILE_ACTIONS = 'actions.tsv'
+        self.FILE_LINKS = 'links.tsv'
+        self.FILE_VISITS = 'visits.tsv'
+        self.DELIMITER = "\t"
+        self.ACTIONS = Actions()
+        self.LINKS = Links()
 
-def run():
-    logging.debug(DIR_OLD)
-    logging.debug(DIR_NEW)
+    def _process_file_with_function(self, filepath, function, limit=None):
+        '''Process the lines in contained in filepath with function.'''
+        if not limit: # Enable part processing of file
+            limit = 1000
+        logging.info('Processing file: %s'%filepath)
+        logging.info('Using function: %s'%function)
+        logging.info('Number of lines limited to: %s'%limit)
+        converted = 0 # lines that converted okay
+        failed = 0 # attempts at conversion that failed
+        skipped = 0 # lines that caused CSV errors
+        with open(filepath, 'rb') as infile:
+            #lines = csv.DictReader(infile, delimiter=self.DELIMITER)
+            lines = csv.reader(infile, delimiter=self.DELIMITER)
+            header = lines.next() # assume data has a header row
+            logging.debug('Header: %s'%header)
+            # Cannot use "for line in lines:" because of binary output
+            # in column 2 raises an error. Wrapping it in a try block
+            # stops processing of further lines. Hence this:
+            while True:
+                try:
+                    if converted == limit:
+                        break
+                    line = lines.next()
+                    if function(line):
+                        converted += 1
+                    else:
+                        failed += 1
+                except csv.Error:
+                    self.check_line_error(line)
+                    skipped += 1
+                except StopIteration:
+                    break
+        logging.info('Number of lines converted: %s'%converted)
+        logging.info('Number of lines that failed to convert: %s'%failed)
+        logging.info('Number lines skipped: %s'%skipped)
+        if skipped > 20:
+            logging.warn('Skipped lines were greater than 20.')
     
+    def check_line_error(self, line):
+        pass
+        #logging.debug('WARNING, the following line was skipped.')
+        #logging.debug(line)
+        #TODO: Check why lines cause a CSV error. Binary field 
+        
+    def process_actions(self):
+        logging.info('Processing actions.')
+        filepath = os.path.join(self.DIR_OLD, self.FILE_ACTIONS)
+        function = self.ACTIONS.process_line
+        self._process_file_with_function(filepath, function)
+        self.ACTIONS.report()
+        outfile = os.path.join(self.DIR_NEW, '%s.tsv'%self.FILE_ACTIONS) 
+        self.ACTIONS.save(outfile)
+    
+    def process_links(self):
+        logging.info('Processing links.')
+        filepath = os.path.join(self.DIR_OLD, self.FILE_LINKS)
+        function = self.LINKS.process_line
+        self._process_file_with_function(filepath, function)
+        outfile = os.path.join(self.DIR_NEW, '%s.tsv'%self.FILE_LINKS) 
+        self.LINKS.save(outfile)
+
+    def run_all(self):
+        logging.debug(self.DIR_OLD)
+        logging.debug(self.DIR_NEW)
+        self.process_actions()
+        self.process_links()
+        
+class Actions(object):
+    def __init__(self):
+        self.DATA = list() # raw data to be saved
+        self.CODES = dict() # codes to use for custom variables
+        self.CODES_FOUND = 0
+        self.LINES_WITHOUT_CODES = 0
+        
+    def process_line(self, line):
+        return True
+    
+    def report(self):
+        logging.info('Codes extracted: %s'%self.CODES_FOUND)
+        logging.info('Lines without codes %s:'%self.LINES_WITHOUT_CODES)
+        
+    def save(self, filepath):
+        logging.info('Saving actions to: %s'%filepath)
+
+class Links(object):
+    def __init__(self):
+        self.DATA = list()
+        
+    def process_line(self, line):
+        #process line and save results
+        #self.DATA.append(line)
+        return True
+    
+    def save(self, filepath):
+        logging.info('Saving actions to: %s'%filepath)
+            
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
-    run()
+    logging.basicConfig(level=logging.INFO)
+    c = Converter()
+    c.run_all()
