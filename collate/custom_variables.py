cb58b359919823f50f3241169618a32b005a7a7d
Martin Comina <martin.comina@bodleian.ox.ac.uk>
2014-02-20 11:07:11 +0000

    Use central Piwik config.

--- collate/custom_variables.py
+++ collate/custom_variables.py
@@ -7,29 +7,23 @@ import dbengine
 class Populate(object):
     '''Take existing data and populate custom variables.'''
     def __init__(self):
-        self.FIELD_TO_POPULATE = '' # field to put data
+        self.CONFIG = None # tables and fields to use
         self.CONNECTION = None # in this location
-    
-    def config_variable(self, number=5, usevalue=True):
-        '''Set custom variable to use the numbered value column.'''
-        if number not in range (1, 6): # Piwik has upto 5 custom vars
-            number = 5
-        cat = 'v' # we will use the "value column of this custom var"
-        if not usevalue:
-            cat = 'k' # but you can use the key column instead.
-        self.FIELD_TO_POPULATE = 'custom_var_%s%s'%(cat, number)
-        logging.debug('Using field: %s'%self.FIELD_TO_POPULATE)
+        self.setup()
         
     def setup(self):
         '''Setup the connection to the system being populated.'''
         source = dbsources.ReadWriteDB()
         source.setup_source1()
         host, username, password, database = source.get_settings()
+        self.CONFIG = dbengine.PiwikConfig()
         self.CONNECTION = dbengine.Connection()
         self.CONNECTION.setup(host, username, password, database)
         
     def sql_count_customvar(self):
-        return "SELECT COUNT(%s) FROM piwik_log_link_visit_action"%self.FIELD_TO_POPULATE
+        count = self.CONFIG.FIELD_CUSTOM_VARS_SCODE
+        table = self.CONFIG.TABLE_CUSTOM_VARS_STORE 
+        return "SELECT COUNT(%s) FROM %s"%(count, table)
     
     def count_existing(self):
         '''Return the number of custom variables that exist.'''
@@ -40,8 +34,6 @@ class Populate(object):
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     p = Populate()
-    p.config_variable()
-    p.setup()
     existing = p.count_existing()
     if existing:
         logging.critical('Data EXISTS in custom variables to populate.')
